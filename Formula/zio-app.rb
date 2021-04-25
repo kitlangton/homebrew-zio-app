04b77265ed4578845152ec93d1a8ca52a97a3aba
Kit Langton <kitlangton@gmail.com>
2021-04-25 16:38:40 -0700

    zio-app 0.1.5
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/zio-app.rb
+++ Formula/zio-app.rb
@@ -1,9 +1,9 @@
 class ZioApp < Formula
   desc "Create ZIO apps quickly."
   homepage "https://github.com/kitlangton/zio-app"
-  url "https://github.com/kitlangton/zio-app/releases/download/v0.1.4/zio-app-x86_64-apple-darwin.gz"
-  sha256 "b8736dc845e8a93d5064f003b1e802fe402049854e674feea1de44ecf1e01506"
-  version "0.1.4"
+  url "https://github.com/kitlangton/zio-app/releases/download/v0.1.5/zio-app-x86_64-apple-darwin.gz"
+  sha256 "5dd1e82574d66319ef1feab29134b848f81ac9fcc5e53a33e6aafecf20b4e557"
+  version "0.1.5"
 
   def install
     bin.install "zio-app"
