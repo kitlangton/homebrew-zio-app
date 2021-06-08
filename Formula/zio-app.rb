adeb76805e3cc54f4da07df126f5160fb8797ff5
Kit Langton <kitlangton@gmail.com>
2021-06-08 16:56:02 -0700

    Update zio-app.rb

--- Formula/zio-app.rb
+++ Formula/zio-app.rb
@@ -1,9 +1,9 @@
 class ZioApp < Formula
   desc "Create ZIO apps quickly."
   homepage "https://github.com/kitlangton/zio-app"
-  url "https://github.com/kitlangton/zio-app/releases/download/v0.1.14/zio-app.gz"
-  sha256 "209c89b59d5acc2f7f3754c05ec075c933bdad4861118442d52ee7e60b9ef6f5"
-  version "0.1.14"
+  url "https://github.com/kitlangton/zio-app/releases/download/v0.2.4/zio-app.gz"
+  sha256 "081e97c14d70822cf6e8b4b7365db667de41042bb9e6c8a4db4542dae0291de9"
+  version "0.2.4"
 
   def install
     bin.install "zio-app"
